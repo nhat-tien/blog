cd5dff203fee6aa2e74bfda98605db41c288877a
Nhat Tien <ledangnhattien04@gmail.com>
2025-06-02 10:11:06 +0700

    style:

---
+++ themes/cozy-garden/assets/js/decoration-code-block.js
@@ -0,0 +1,9 @@
+const codeBlocks = document.querySelectorAll(".chroma");
+
+codeBlocks.forEach(e => {
+  const language = e.querySelector("code").getAttribute("data-lang");
+  const divLanguage = document.createElement("div");
+  divLanguage.className = "decoration-language";
+  divLanguage.innerText = language;
+  e.appendChild(divLanguage)
+});
